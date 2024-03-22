2b3708c9347959e435bc5a97ab3aa63b359a2fea
jogroe <jonasg2509@gmail.com>
2024-03-22 13:17:06 +0100

    fix deploy

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def add_data():
     return jsonify({'message': 'Données ajoutées avec succès !'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)

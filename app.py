20bb7ebdc4a52063f8bcd97f10a7fa13c94db837
jogroe <jonasg2509@gmail.com>
2024-03-22 10:49:26 +0100

    test deployment

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+data = []
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    return jsonify(data)
+
+@app.route('/data', methods=['POST'])
+def add_data():
+    new_entry = request.json
+    data.append(new_entry)
+    return jsonify({'message': 'Données ajoutées avec succès !'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
